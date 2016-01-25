e6e26b9340def55a7f7a470dfa3aa9ba7b925e5f
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-01-25 15:44:10 -0700

    first pass at testing

---
+++ client/spec/items.spec.ts
@@ -0,0 +1,98 @@
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
+import {Item, AppStore, ItemsService, items, selectedItem} from '../src/items';
+import {Store, provideStore} from '@ngrx/store';
+import {Injector} from 'angular2/core';
+
+import {
+  it,
+  describe,
+  expect,
+  inject,
+  injectAsync,
+  beforeEachProviders,
+  TestComponentBuilder
+} from 'angular2/testing';
+
+describe('Items', () => {
+  let injector, store;
+  beforeEachProviders(() => [
+    provideStore({items, selectedItem}),
+    ItemsService,
+    HTTP_PROVIDERS
+  ]);
+
+  describe('`selectedItem` store', () => {
+    it('returns null by default', () => {
+      let defaultState = selectedItem(undefined, {type: 'random', payload: {}});
+
+      expect(defaultState).toBeNull();
+    });
+
+    it('`SELECT_ITEM` returns the provided payload', () => {
+      let selectItem = selectedItem(undefined, {type: 'SELECT_ITEM', payload: 'payload'});
+
+      expect(selectItem).toBe('payload');
+    });
+  });
+
+  describe('`items` store', () => {
+    let initialState = [
+      { id: 0, name: 'First Item' },
+      { id: 1, name: 'Second Item' }
+    ];
+
+    it('returns an empty array by default', () => {
+      let defaultState = items(undefined, {type: 'random', payload: {}});
+
+      expect(defaultState).toEqual([]);
+    });
+
+    it('`ADD_ITEMS`', () => {
+      let payload = initialState,
+          stateItems = items([], {type: 'ADD_ITEMS', payload: payload});
+
+      expect(stateItems).toEqual(payload);
+    });
+
+    it('`CREATE_ITEM`', () => {
+      let payload = {id: 2, name: 'added item'},
+          result = [...initialState, payload],
+          stateItems = items(initialState, {type: 'CREATE_ITEM', payload: payload});
+
+      expect(stateItems).toEqual(result);
+    });
+
+    it('`UPDATE_ITEM`', () => {
+      let payload = { id: 1, name: 'Updated Item' },
+          result = [ initialState[0], { id: 1, name: 'Updated Item' } ],
+          stateItems = items(initialState, {type: 'UPDATE_ITEM', payload: payload});
+
+      expect(stateItems).toEqual(result);
+    });
+
+    it('`DELETE_ITEM`', () => {
+      let payload = { id: 0 },
+          result = [ initialState[1] ],
+          stateItems = items(initialState, {type: 'DELETE_ITEM', payload: payload});
+
+      expect(stateItems).toEqual(result);
+    });
+  });
+
+  describe('itemsService', () => {
+    it('items are defined', inject([ ItemsService ], (service, http) => {
+      expect(service.items).toBeDefined();
+    }));
+
+    // Won't run...for some reason the Http provider is not getting attached to the class properly
+    // it('#loadItems', inject([ ItemsService ], (service) => {
+    //   spyOn(service.http, 'get');
+    //   spyOn(service.store, 'dispatch');
+    //
+    //   service.loadItems();
+    //
+    //   expect(service.http.get).toHaveBeenCalled();
+    //   expect(service.store.dispatch).toHaveBeenCalled();
+    // }));
+  });
+});
