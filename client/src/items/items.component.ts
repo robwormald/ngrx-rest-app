880b27f6bc33fb76941c18fd0127798622f2007d
Lukas Ruebbelke <simpul@gmail.com>
2016-03-17 23:52:39 -0500

    fixing this template

--- client/src/items/items.component.ts
+++ client/src/items/items.component.ts
@@ -1,23 +1,24 @@
-import {Component, Input, Output, EventEmitter, OnInit} from 'angular2/core';
-import {ItemsService, Item} from './../common/services/items.service.ts';
+import {Component, Input, Output, EventEmitter} from 'angular2/core';
+import {Observable} from "rxjs/Observable";
+import {Store} from '@ngrx/store';
+import {ItemsService, Item, AppStore} from './../common/services/items.service.ts';
 import {ItemsList} from './items-list.component';
 import {ItemDetail} from './item-detail.component';
+
 
 @Component({
   selector: 'items',
   template: `
   <div class="mdl-grid items">
     <div class="mdl-cell mdl-cell--6-col">
-      <items-list [items]="items"
-      (selected)="selectItem($event)" (deleted)="deleteItem($event)">
+      <items-list [items]="items | async"
+        (selected)="selectItem($event)" (deleted)="deleteItem($event)">
       </items-list>
     </div>
     <div class="mdl-cell mdl-cell--6-col">
       <item-detail
-      (saved)="saveItem($event)" (cancelled)="resetItem($event)"
-      [item]="selectedItem">
-        Select an Item
-      </item-detail>
+        (saved)="saveItem($event)" (cancelled)="resetItem($event)"
+        [item]="selectedItem | async">Select an Item</item-detail>
     </div>
   </div>
   `,
@@ -29,58 +30,37 @@ import {ItemDetail} from './item-detail.component';
   providers: [ItemsService],
   directives: [ItemsList, ItemDetail]
 })
-export class Items implements OnInit {
-  items: Array<Item>;
-  selectedItem: Item;
+export class Items {
+  items: Observable<Array<Item>>;
+  selectedItem: Observable<Item>;
 
-  constructor(private itemsService: ItemsService) {}
+  constructor(private itemsService: ItemsService, private store: Store<AppStore>) {
+    this.items = itemsService.items;
+    this.selectedItem = store.select('selectedItem');
+    this.selectedItem.subscribe(v => console.log(v));
 
-  ngOnInit() {
-    this.itemsService.loadItems()
-      .then(items => {
-        this.items = items;
-      });
+    itemsService.loadItems();
   }
 
   resetItem() {
     let emptyItem: Item = {id: null, name: '', description: ''};
-    this.selectedItem = emptyItem;
+    this.store.dispatch({type: 'SELECT_ITEM', payload: emptyItem});
   }
 
   selectItem(item: Item) {
-    this.selectedItem = item;
+    this.store.dispatch({type: 'SELECT_ITEM', payload: item});
   }
 
   saveItem(item: Item) {
-    this.itemsService.saveItem(item)
-      .then(responseItem => {
-        if (item.id) {
-          this.replaceItem(responseItem);
-        } else {
-          this.pushItem(responseItem);
-        }
-      });
+    this.itemsService.saveItem(item);
 
     // Generally, we would want to wait for the result of `itemsService.saveItem`
     // before resetting the current item.
     this.resetItem();
   }
 
-  replaceItem(item: Item) {
-    this.items = this.items.map(mapItem => {
-      return mapItem.id === item.id ? item : mapItem;
-    });
-  }
-
-  pushItem(item: Item) {
-    this.items.push(item);
-  }
-
   deleteItem(item: Item) {
-    this.itemsService.deleteItem(item)
-      .then(() => {
-        this.items.splice(this.items.indexOf(item), 1);
-      });
+    this.itemsService.deleteItem(item);
 
     // Generally, we would want to wait for the result of `itemsService.deleteItem`
     // before resetting the current item.
