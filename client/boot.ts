5f48c52567a24773b5baf2fe388a52fdbab647dc
Hendrixer <willscottmoss@gmail.com>
2016-03-18 01:04:03 -0500

    add(devtools)

--- client/boot.ts
+++ client/boot.ts
@@ -4,6 +4,8 @@ import 'zone.js/dist/zone-microtask';
 import {bootstrap} from 'angular2/platform/browser';
 import {provide} from 'angular2/core';
 import {HTTP_PROVIDERS} from 'angular2/http';
+import {instrumentStore} from '@ngrx/devtools'
+
 import {
   ROUTER_PROVIDERS,
   Location,
@@ -21,5 +23,6 @@ bootstrap(App, [
   ROUTER_PROVIDERS,
   provide(LocationStrategy, {useClass: HashLocationStrategy}),
   ItemsService,
-  provideStore({items, selectedItem})
+  provideStore({items, selectedItem}),
+  instrumentStore()
 ]);
--- client/src/app.ts
+++ client/src/app.ts
@@ -2,11 +2,12 @@ import {Component, Input, Output, EventEmitter, OnInit} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, Router} from 'angular2/router';
 import {Items} from './items/items.component';
 import {Widgets} from './widgets/widgets.component';
+import {Devtools} from '@ngrx/devtools';
 
 @Component({
   selector: 'my-app',
   template: require('./app.html'),
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES, Devtools]
 })
 @RouteConfig([
   {path: '/items', name: 'Items', component: Items, useAsDefault: true},
